97145447e050b813d1d6c11143eeaf27c4bba792
jeonghyeonee <jlh1218@naver.com>
2022-05-25 11:03:24 +0900

    MAX를 넘어감

--- ProgrammingAssignment3/src/dining.java
+++ ProgrammingAssignment3/src/dining.java
@@ -1,57 +1,111 @@
 // Use java threads to simulate the Dining Philosophers Problem
 // YOUR NAME HERE.  Programming assignment 2 (from ece.gatech.edu) */
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.concurrent.Semaphore;
 
-class dining
-{
-    public static Philosopher[] philosopher = new Philosopher[5];
-    public static void main(String args[])
-    {
+class dining {
+    public static void main(String args[]) throws InterruptedException {
         System.out.println("Starting the Dining Philosophers Simulation\n");
         miscsubs.InitializeChecking();
         // Your code here...
 
-        ArrayList<Thread> threadArr = new ArrayList<>();
+        Philosopher[] philosophers = new Philosopher[miscsubs.NUMBER_PHILOSOPHERS];
 
-        for (int i=0; i<5; i++){
-            Thread thread = new Thread(new Philosopher(i));
-            threadArr.add(thread);
+        PhilosopherMonitor monitor = new PhilosopherMonitor(miscsubs.NUMBER_PHILOSOPHERS);
+
+
+        for (int i = 0; i < miscsubs.NUMBER_PHILOSOPHERS; i++) {
+            philosophers[i] = new Philosopher(i, monitor);
+            new Thread(philosophers[i]).start();
         }
 
+        while(miscsubs.TotalEats < miscsubs.MAX_EATS){
+        }
 
-
-
-
-
-
-
+        System.out.println();
 
         // End of your code
         System.out.println("Simulation Ends..");
         miscsubs.LogResults();
-    }
-
-    public static class Philosopher implements Runnable{
-
-        public int[] chopstick = new int[5];
-
-        static Object both = new Object();
-        int id;
-
-        Philosopher(int i){
-            id = i;
         }
 
+}
 
-        @Override
-        public void run() {
+class Philosopher implements Runnable{
 
+    private int id;
+    private PhilosopherMonitor monitor;
+
+    public Philosopher(int id, PhilosopherMonitor monitor) {
+        this.id = id;
+        this.monitor = monitor;
+    }
+
+
+    public void run(){
+        try{
+            while(miscsubs.TotalEats < miscsubs.MAX_EATS){
+                think(); // thinking
+                monitor.pickUp(id);     // eating start
+                eat(id);
+                monitor.putDown(id);    // eating finish
+            }
+        } catch(InterruptedException e){
+            System.out.println("Philosopher " + id + " was interrupted.\n");
+        }
+
+    }
+
+    
+    private void think(){
+        miscsubs.RandomDelay();
+    }
+
+
+    public void eat(int id){
+        miscsubs.StartEating(id);
+        miscsubs.RandomDelay();
+        miscsubs.DoneEating(id);
+    }
+
+}
+
+class PhilosopherMonitor{
+    private enum State {THINKING, HUNGRY, EATING};
+
+    private State[] philosopherState;
+
+    public PhilosopherMonitor(int num){
+        philosopherState = new State[num];
+        for (int i = 0; i < num; i++){
+            philosopherState[i] = State.THINKING;
         }
     }
 
-};
+    public synchronized void pickUp(int id) throws InterruptedException{
+        philosopherState[id] = State.HUNGRY;
+
+        while(neighborEating(id)){
+            wait();
+        }
+        philosopherState[id] = State.EATING;
+    }
 
 
+    private boolean neighborEating(int id){
+        int num = philosopherState.length;
+        if(philosopherState[(id + 1) % num] == State.EATING){
+            return true;
+        }
+
+        if(philosopherState[(id + num - 1) % num] == State.EATING){
+            return true;
+        }
+
+        return false;
+    }
+
+    public synchronized void putDown(int id) throws InterruptedException{
+        philosopherState[id] = State.THINKING;
+        notifyAll();
+    }
+}
